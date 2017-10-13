4d5de99f2b5b4c10e941ca58bc1798ecfb047a5f
Colten Buzard <buzardc@aldenv168.allegheny.edu>
2017-10-13 09:02:20 -0400

    Materials for lab 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,22 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/** This program takes a string of dna and prints the compliment of that dna.
+ *
+ *
+ */
 public class ManipulateDna {
+  /** This program will prompt the user to type a string of dna
+   * Then it will print the complement of that dna, appropriately labeled.
+   *
+   */
 
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Colten Buzard " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -20,18 +29,66 @@ public class ManipulateDna {
     } catch (FileNotFoundException noFile) {
       System.out.println("Unable to locate file");
     }
-    // Step One: Read in and display the chosen DNA string
+    // Step One: Read in and display the chosen dna string
+    String input = scanner.nextLine();
+    final String dna = input.toUpperCase();
+    System.out.println("Okay, I am going to manipulate the DNA string \"" + dna + "\".\n");
 
-    // Step Two: Compute the complement of the DNA String
+    // Step Two: Compute the complement of the dna String
+    String dnacomp = dna;
 
-    // Step Three: Insert a randomly chosen DNA letter into the DNA String
+    /* Have to change each letter to a different variable so that it doesn't change it twice.
+    T = X
+    A = Y
+    C = Z
+    G = W
 
-    // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
+A-T
+C-G
+In this step I'm taking each letter, then giving it a psuedo letter so that it
+ doesn't get replaced again.
+    */
+    dnacomp = dnacomp.replace('T','X');
+    dnacomp = dnacomp.replace('A','Y');
+    dnacomp = dnacomp.replace('C','Z');
+    dnacomp = dnacomp.replace('G','W');
+    // Converting each pseudo letter to it's compliment
+    dnacomp = dnacomp.replace('X','A');
+    dnacomp = dnacomp.replace('Y','T');
+    dnacomp = dnacomp.replace('Z','G');
+    dnacomp = dnacomp.replace('W','C');
 
-    // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
+    // Printing the compliment
+    System.out.println("Enter a string containing only C, G, T, and A: " + dna);
+    System.out.println("Complement of ACTG is " + dnacomp);
 
+    // Step Three: Insert a randomly chosen dna letter into the dna String
+    String allowed = "CGTA";
+    Random ran = new Random();
+    int randomNum = ran.nextInt(4);
+    char randomletter = allowed.charAt(randomNum);
+    System.out.println("Inserting " + randomletter + " at position 0 gives "
+        + randomletter + dna);
+
+    // Step Four: Delete a dna letter from a randomly chosen position in the dna string
+    int randompos = ran.nextInt(dna.length());
+    String dnadelete = ("" + dna.substring(0, randompos)
+        + dna.substring(randompos + 1, dna.length()));
+    
+    System.out.println("Deleting from position " + (randompos + 1) + " gives " + dnadelete);
+    // Step Five: Change a random position in the dna String to a randomly chosen dna letter
+    // randomNum2 chooses a random number from dna; In dnasub I'll remove this 
+    int randomNum2 = ran.nextInt(dna.length());
+    // randomNum3 will be used to create randomletter2
+    int randomNum3 = ran.nextInt(dna.length());
+    char randomletter2 = dna.charAt(randomNum3);
+
+    String dnasub = ("" + dna.substring(0, randomNum2) + randomletter2
+        + dna.substring(randomNum2 + 1, dna.length()));
+
+    System.out.println("Changing position " + (randomNum2 + 1) + " gives " + dnasub);
     // Step Six: Display a final thankyou message
-
+    System.out.println("\nThanks for using the ManipulateDna program.\nHave an awesome day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,18 @@
 # Reflection by
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. The first step
+involved replacing the letters within a string of DNA with their respective
+compliments. After my first attempt, I realized that simply replacing each
+letter with it's compliment would cause a dilemna.
+
+After replacing the first complimentary letter, I would quickly change it right
+back to its original letter in the next command. To get around this dilemna I
+gave each letter in the dna string a pseudo letter so that it would not get
+double replaced.The hardest step in this assignment was deleting a random dna
+letter from the input string and then replacing it with a random letter.
+
+I understood how to choose a random character from the string from previous
+labs, but it took me a while to understand how to remove it from the string. To
+delete it from the string I created two seperate substrings that excluded the
+chosen letter. When replacing it with a new letter I simply created a new
+random letter and added it in between the two strings.
